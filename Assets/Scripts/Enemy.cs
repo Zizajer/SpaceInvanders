78abd0afbe53a76732c761cbaed03f30b0122429
Piotr <zizi5673@gmail.com>
2019-05-16 01:41:32 +0200

    Added special enemy the UFO.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -35,6 +35,7 @@ public class Enemy : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    
 
     void Shoot()
     {
---
+++ Assets/Scripts/SpecialEnemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialEnemy : MonoBehaviour
+{
+    public float MovementSpeed;
+    public ParticleSystem ExplosionEffect;
+    public SpriteRenderer spriteRenderer;
+    public float ScoreAmountIncrease;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(MovementSpeed * Time.deltaTime, 0, 0);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "PlayerBullet")
+        {
+            ExplosionEffect.startColor = spriteRenderer.color;
+            ParticleSystem particleEffect = Instantiate(ExplosionEffect, transform.position, new Quaternion(0, 0, 0, 0));
+            Destroy(particleEffect.gameObject, particleEffect.duration);
+            GameObject.FindGameObjectWithTag("UI").GetComponent<UIManager>().IncreaseScore(ScoreAmountIncrease);
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
